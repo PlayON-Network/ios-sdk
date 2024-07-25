34219f2fe36853c0984ad5db8a010d102daa0b8a
David Arias <dav.surf@gmail.com>
2024-07-25 12:19:12 +0200

    chore: Add fantasy view controller getter

--- Classes/PlayonNetwork.swift
+++ Classes/PlayonNetwork.swift
@@ -2,9 +2,14 @@ import Flutter
 
 public class PlayonNetwork {
   private static var _engine: PlayonNetworkEngine?;
+  private static var _fantasyViewController: UIViewController?;
 
   public static var engine: PlayonNetworkEngine? {
     return self._engine;
+  }
+
+  public static var fantasyViewController: UIViewController? {
+    return self._fantasyViewController;
   }
 
   public static func initializeEngine(
@@ -24,15 +29,19 @@ public class PlayonNetwork {
   }
 
   public static func createFantasyViewController() -> UIViewController {
-    let viewController = FlutterViewController(
-      engine: self._engine!.flutterEngine,
-      nibName: nil,
-      bundle: nil
-    );
+    if (self._fantasyViewController == nil) {
+      let viewController = FlutterViewController(
+        engine: self._engine!.flutterEngine,
+        nibName: nil,
+        bundle: nil
+      );
 
-    viewController.modalPresentationStyle = .overCurrentContext;
-    viewController.isViewOpaque = false;
+      viewController.modalPresentationStyle = .overCurrentContext;
+      viewController.isViewOpaque = false;
 
-    return viewController;
+      self._fantasyViewController = viewController;
+    }
+
+    return self._fantasyViewController!;
   }
 }
